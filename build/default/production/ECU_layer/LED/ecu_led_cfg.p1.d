74dc7a7344d065bc0aa3e201d198c0f85fe77748
Abdelrahman Mansour <108426940+Abdrlrahman-Mansour@users.noreply.github.com>
2024-08-05 06:40:54 +0300

    Delete build/default/production directory

--- build/default/production/ECU_layer/LED/ecu_led_cfg.p1.d
+++
@@ -1,3 +0,0 @@
-build/default/production/ECU_layer/LED/ecu_led_cfg.p1:  \
-ECU_layer/LED/ecu_led_cfg.c  \
-ECU_layer/LED/ecu_led_cfg.h 
--- build/default/production/MCAL_layer/GPIO/hal_gpio_cfg.p1.d
+++
@@ -1,3 +0,0 @@
-build/default/production/MCAL_layer/GPIO/hal_gpio_cfg.p1:  \
-MCAL_layer/GPIO/hal_gpio_cfg.c  \
-MCAL_layer/GPIO/hal_gpio_cfg.h 
